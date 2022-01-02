508ea86680aa292cc0b2e871f9e0bda230f9ac37
Joey <jaschuett@wisc.edu>
2022-01-02 02:48:44 -0600

    upgrade interval

--- JavaScript/Script1.js
+++ JavaScript/Script1.js
@@ -3,13 +3,33 @@
 var ppp = Math.E;
 var tick = 0;
 var val = 0;
-var lamb = 10
-var millisecondsToWait = 100;
+var millisecondsToWait = 1000;
+var lamb = 1 / millisecondsToWait
+var avg = 0;
+var intCost = 10
 function main() {
-    u = Math.random
-    x = Math.log(1 - u) / (-lamb)
-    setTimeout(main, millisecondsToWait)
+    u = Math.random()
+    x = Math.log(1 - u) / (-1*lamb)
     val++
-    document.getElementById('b1').innerHTML = val
-    main()
+    document.getElementById('b1').innerHTML = Math.round(val)
+    document.getElementById('bInt').innerHTML = "Upgrade Interval (" + Math.round(intCost) + ")"
+    avg *= (val - 1)
+    avg += x
+    avg /= val
+
+    console.log(avg, x)
+    setTimeout(main, x)
+}
+
+function upgradeInterval() {
+    if (val >= intCost) {
+        millisecondsToWait *= 0.95
+        lamb = 1 / millisecondsToWait
+        val -= intCost
+        intCost *= 1.05
+    }
+}
+
+function upgradePower() {
+
 }
